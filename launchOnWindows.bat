01bfcbf0a71d34fdc6b166b3c3cb8edae50e6d43
Hanne Maes <maes.hanne@gmail.com>
2023-02-01 11:37:31 +0100

    new windows launcher

--- launchOnWindows.bat
+++ launchOnWindows.bat
@@ -1,3 +1,6 @@
+:: Samanthe GPT
+:: To autostart: move 'launchOnWindows.bat' and 'launchOnWindowsNoTerminal.vbs' to the Windows startup folder
+
 cd "C:\Users\hannemaes\Documents\Code\SamanthaGPT\electron-app"
 npm start
 pause
